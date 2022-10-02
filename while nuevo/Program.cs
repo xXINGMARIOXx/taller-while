21d8d96dc61a4e1b0de15aedf8b54ff4c9101be0
xXINGMARIOXx <mario.banol392@pascualbravo.edu.co>
2022-10-01 22:22:21 -0500

    Agregar archivos de proyecto.

---
+++ while nuevo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_nuevo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0, i=1,i2=0, positivos=1;
+            int ingresado = 0;
+            
+            while (i <= 6)
+            {
+                Console.WriteLine("Ingresar número ");
+                ingresado = int.Parse(Console.ReadLine());
+                if (ingresado < 0)
+                {
+                    suma += ingresado;
+                }
+                else if(ingresado > 0)
+                { 
+                    positivos+= ingresado;
+
+                    i2+=1 ;
+
+
+                }
+                
+                else
+                {
+                    i++; 
+                    continue;
+                }
+
+
+                i++;
+
+            }
+            Console.WriteLine("la suma de los negativos = "+ suma +"\nPromedio "+ positivos % i2 );
+            Console.ReadKey();
+        }
+    }
+}
